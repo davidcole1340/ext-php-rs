0211148a1ab7505ece98cd5539cf7edddbda3600
David Cole <david.cole1340@gmail.com>
2021-03-09 21:02:56 +1300

    hide public c functions from docs (wrapped by macros)

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,12 @@ pub mod php;
 
 // Bindings used by macros. Used so that the rest of the bindings can be hidden with `pub(crate)`.
 extern "C" {
+    #[doc(hidden)]
     pub fn php_info_print_table_header(num_cols: ::std::os::raw::c_int, ...);
+    #[doc(hidden)]
     pub fn php_info_print_table_row(num_cols: ::std::os::raw::c_int, ...);
+    #[doc(hidden)]
     pub fn php_info_print_table_start();
+    #[doc(hidden)]
     pub fn php_info_print_table_end();
 }
