68aecf58163f6d6bbc68c371e2c38b2d77595bae
David <david.cole1340@gmail.com>
2021-03-16 10:55:22 +1300

    Fixed incorrect build ID not letting extension run (#15)

--- wrapper.h
+++ wrapper.h
@@ -1,3 +1,9 @@
 #include "php.h"
 #include "ext/standard/info.h"
-#include "zend_extensions.h"
+#include "zend_exceptions.h"
+
+// Need to define these as empty strings to ensure that
+// bindgen generates them.
+#define ZEND_BUILD_DEBUG ""
+#define ZEND_BUILD_SYSTEM ""
+#define ZEND_BUILD_EXTRA ""
