beb1d7fd32759650923ee87ef629c0ec975ea974
davidcole1340 <david.cole1340@gmail.com>
2021-05-21 03:11:27 +0000

    Deploying to gh-pages from  @ de326e31712f5e67282e0118748b324250da64d6 🚀

--- ext_php_rs/php/types/string/sidebar-items.js
+++ ext_php_rs/php/types/string/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"type":[["ZendString","String type used in the Zend internals. The actual size of the ‘string’ differs, as the end of this struct is only 1 char long, but the length inside the struct defines how many characters are in the string."]]});
+initSidebarItems({"struct":[["ZendString","A wrapper around the [`zend_string`] used within the Zend API. Essentially a C string, except that the structure contains the length of the string as well as the string being refcounted."]]});
