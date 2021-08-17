eafeaa6216ba0b875bda70c7284d31aa7541e05f
David Cole <david.cole1340@gmail.com>
2021-08-18 01:03:22 +1200

    Fix clippy lint

--- ext-php-rs-derive/src/function.rs
+++ ext-php-rs-derive/src/function.rs
@@ -323,7 +323,7 @@ impl Arg {
 
         if let Some(default) = self.default.as_ref() {
             // `bool`s are not literals - need to use Ident.
-            let val = syn::parse_str::<Literal>(&default)
+            let val = syn::parse_str::<Literal>(default)
                 .map(|lit| lit.to_token_stream())
                 .or_else(|_| Ident::from_string(default).map(|ident| ident.to_token_stream()))
                 .unwrap_or(quote! { Default::default() });
