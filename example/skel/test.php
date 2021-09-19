c7b732959c49bc93507f74f7da3ce51b02c53165
David Cole <david.cole1340@gmail.com>
2021-09-20 02:10:10 +1200

    Remove `ZendHashTable` wrapper
    Replaced by returning references to the actual hashtable, and having a
    new type `OwnedHashTable` when creating a new hashtable.

--- example/skel/test.php
+++ example/skel/test.php
@@ -1,10 +1,14 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 test_zval(['hello' => 'world']);
 =======
 test_zval();
 >>>>>>> 712aded56 (Call zval destructor when changing zval type and dropping)
+=======
+test_zval(['hello' => 'world']);
+>>>>>>> 3646da741 (Remove `ZendHashTable` wrapper)
 //include 'vendor/autoload.php';
 
 //$ext = new ReflectionExtension('skel');
