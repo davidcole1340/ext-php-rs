854f1d66cb6d5d0ffda5d820585ace47d3b064b8
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-01-16 18:09:53 +0100

    tests: show more informations

--- tests/src/integration/_utils.php
+++ tests/src/integration/_utils.php
@@ -2,8 +2,6 @@
 // Active assert and make it quiet
 assert_options(ASSERT_ACTIVE, 1);
 assert_options(ASSERT_WARNING, 0);
-// Set up the callback
-assert_options(ASSERT_CALLBACK, fn () => exit(1));
 
 function assert_exception_thrown(callable $callback): void
 {
