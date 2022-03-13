3990ad34ffd70a488caa64097cbd6ffe2dfed1b6
David Cole <david.cole1340@gmail.com>
2022-03-13 19:30:07 +1300

    Start work on cross-platform build script

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use std::{
+    borrow::Cow,
     convert::TryInto,
     env,
     fs::File,
@@ -10,13 +11,135 @@ use std::{
 
 use anyhow::{anyhow, bail, Context, Result};
 use bindgen::RustTarget;
-use regex::Regex;
 
 const MIN_PHP_API_VER: u32 = 20200930;
 const MAX_PHP_API_VER: u32 = 20210902;
 
 const USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 const DEFINES: &[&str] = &["ZEND_WIN32", "WINDOWS", "PHP_WIN32", "WIN32"];
+
+trait PHPProvider<'a>: Sized {
+    fn new(info: &'a PHPInfo) -> Result<Self>;
+    fn get_includes(&self) -> Result<Vec<PathBuf>>;
+    fn get_defines(&self) -> Result<Vec<(&'static str, &'static str)>>;
+    fn write_bindings(&self, bindings: String, writer: &mut impl Write) -> Result<()> {
+        for line in bindings.lines() {
+            writeln!(writer, "{}", line)?;
+        }
+        Ok(())
+    }
+    fn print_extra_link_args(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+struct Windows<'a> {
+    info: &'a PHPInfo,
+    devel: DevelPack,
+}
+
+impl<'a> Windows<'a> {
+    fn get_php_lib_name(&self) -> Result<String> {
+        Ok(self
+            .devel
+            .php_lib()
+            .file_stem()
+            .context("Failed to get PHP library name")?
+            .to_string_lossy()
+            .to_string())
+    }
+}
+
+impl<'a> PHPProvider<'a> for Windows<'a> {
+    fn new(info: &'a PHPInfo) -> Result<Self> {
+        let version = info.version()?;
+        let is_zts = info.thread_safety()?;
+        let arch = info.architecture()?;
+        let devel = DevelPack::new(version, is_zts, arch)?;
+        Ok(Self { info, devel })
+    }
+
+    fn get_includes(&self) -> Result<Vec<PathBuf>> {
+        Ok(self.devel.include_paths())
+    }
+
+    fn get_defines(&self) -> Result<Vec<(&'static str, &'static str)>> {
+        Ok(vec![
+            ("ZEND_WIN32", "1"),
+            ("PHP_WIN32", "1"),
+            ("WINDOWS", "1"),
+            ("WIN32", "1"),
+            ("ZEND_DEBUG", if self.info.debug()? { "1" } else { "0" }),
+        ])
+    }
+
+    fn write_bindings(&self, bindings: String, writer: &mut impl Write) -> Result<()> {
+        // For some reason some symbols don't link without a `#[link(name = "php8")]`
+        // attribute on each extern block. Bindgen doesn't give us the option to add
+        // this so we need to add it manually.
+        let php_lib_name = self.get_php_lib_name()?;
+        for line in bindings.lines() {
+            match &*line {
+                "extern \"C\" {" | "extern \"fastcall\" {" => {
+                    writeln!(writer, "#[link(name = \"{}\")]", php_lib_name)?;
+                }
+                _ => {}
+            }
+            writeln!(writer, "{}", line)?;
+        }
+        Ok(())
+    }
+
+    fn print_extra_link_args(&self) -> Result<()> {
+        let php_lib_name = self.get_php_lib_name()?;
+        let php_lib_search = self
+            .devel
+            .php_lib()
+            .parent()
+            .context("Failed to get PHP library parent folder")?
+            .to_string_lossy()
+            .to_string();
+        println!("cargo:rustc-link-lib=dylib={}", php_lib_name);
+        println!("cargo:rustc-link-search={}", php_lib_search);
+        Ok(())
+    }
+}
+
+struct Unix {}
+
+impl Unix {
+    fn php_config(&self, arg: &str) -> Result<String> {
+        let cmd = Command::new("php-config")
+            .arg(arg)
+            .output()
+            .context("Failed to run `php-config`")?;
+        let stdout = String::from_utf8_lossy(&cmd.stdout);
+        if !cmd.status.success() {
+            let stderr = String::from_utf8_lossy(&cmd.stderr);
+            bail!("Failed to run `php-config`: {} {}", stdout, stderr);
+        }
+        Ok(stdout.to_string())
+    }
+}
+
+impl<'a> PHPProvider<'a> for Unix {
+    fn new(_: &'a PHPInfo) -> Result<Self> {
+        Ok(Self {})
+    }
+
+    fn get_includes(&self) -> Result<Vec<PathBuf>> {
+        Ok(self
+            .php_config("--includes")?
+            .split(' ')
+            .map(|s| s.trim_start_matches("-I"))
+            .map(PathBuf::from)
+            .collect())
+    }
+
+    fn get_defines(&self) -> Result<Vec<(&'static str, &'static str)>> {
+        Ok(vec![])
+    }
+}
 
 /// Attempt to find a `vswhere` binary in the common locations.
 pub fn find_vswhere() -> Option<PathBuf> {
@@ -85,12 +208,8 @@ fn get_linkers(vswhere: &Path) -> Result<Vec<PathBuf>> {
     Ok(linkers)
 }
 
-#[cfg(windows)]
-const WHICH: &str = "where";
-#[cfg(not(windows))]
-const WHICH: &str = "which";
-
 fn find_executable(name: &str) -> Option<PathBuf> {
+    const WHICH: &str = if cfg!(windows) { "where" } else { "which" };
     let cmd = Command::new(WHICH).arg(name).output().ok()?;
     if cmd.status.success() {
         let stdout = String::from_utf8_lossy(&cmd.stdout);
@@ -110,12 +229,60 @@ fn find_php() -> Result<PathBuf> {
     find_executable("php").context("Could not find PHP path. Please ensure `php` is in your PATH or the `PHP` environment variable is set.")
 }
 
-// https://windows.php.net/downloads/releases/php-devel-pack-8.1.3-nts-Win32-vs16-x64.zip
-// https://windows.php.net/downloads/releases/php-devel-pack-8.1.3-Win32-vs16-x64.zip
-
 struct DevelPack(PathBuf);
 
 impl DevelPack {
+    fn new(version: &str, is_zts: bool, arch: &str) -> Result<DevelPack> {
+        let zip_name = format!(
+            "php-devel-pack-{}{}-Win32-{}-{}.zip",
+            version,
+            if is_zts { "" } else { "-nts" },
+            "vs16", /* TODO(david): At the moment all PHPs supported by ext-php-rs use VS16 so
+                     * this is constant. */
+            arch
+        );
+
+        fn download(zip_name: &str, archive: bool) -> Result<PathBuf> {
+            let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
+            let url = format!(
+                "https://windows.php.net/downloads/releases{}/{}",
+                if archive { "/archives" } else { "" },
+                zip_name
+            );
+            let request = reqwest::blocking::ClientBuilder::new()
+                .user_agent(USER_AGENT)
+                .build()
+                .context("Failed to create HTTP client")?
+                .get(url)
+                .send()
+                .context("Failed to download development pack")?;
+            request
+                .error_for_status_ref()
+                .context("Failed to download development pack")?;
+            let bytes = request
+                .bytes()
+                .context("Failed to read content from PHP website")?;
+            let mut content = Cursor::new(bytes);
+            let mut zip_content = zip::read::ZipArchive::new(&mut content)
+                .context("Failed to unzip development pack")?;
+            let inner_name = zip_content
+                .file_names()
+                .next()
+                .and_then(|f| f.split('/').next())
+                .context("Failed to get development pack name")?;
+            let devpack_path = out_dir.join(inner_name);
+            let _ = std::fs::remove_dir_all(&devpack_path);
+            zip_content
+                .extract(&out_dir)
+                .context("Failed to extract devpack to directory")?;
+            Ok(devpack_path)
+        }
+
+        download(&zip_name, false)
+            .or_else(|_| download(&zip_name, true))
+            .map(DevelPack)
+    }
+
     pub fn includes(&self) -> PathBuf {
         self.0.join("include")
     }
@@ -168,67 +335,6 @@ impl DevelPack {
     }
 }
 
-fn download_devel_pack(version: &str, is_zts: bool, arch: &str) -> Result<DevelPack> {
-    let zip_name = format!(
-        "php-devel-pack-{}{}-Win32-{}-{}.zip",
-        version,
-        if is_zts { "" } else { "-nts" },
-        "vs16", /* TODO(david): At the moment all PHPs supported by ext-php-rs use VS16 so this
-                 * is constant. */
-        arch
-    );
-
-    fn download(zip_name: &str, archive: bool) -> Result<PathBuf> {
-        let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
-        let url = format!(
-            "https://windows.php.net/downloads/releases{}/{}",
-            if archive { "/archives" } else { "" },
-            zip_name
-        );
-        let request = reqwest::blocking::ClientBuilder::new()
-            .user_agent(USER_AGENT)
-            .build()
-            .context("Failed to create HTTP client")?
-            .get(url)
-            .send()
-            .context("Failed to download development pack")?;
-        request
-            .error_for_status_ref()
-            .context("Failed to download development pack")?;
-        let bytes = request
-            .bytes()
-            .context("Failed to read content from PHP website")?;
-        let mut content = Cursor::new(bytes);
-        let mut zip_content =
-            zip::read::ZipArchive::new(&mut content).context("Failed to unzip development pack")?;
-        let inner_name = zip_content
-            .file_names()
-            .next()
-            .and_then(|f| f.split('/').next())
-            .context("Failed to get development pack name")?;
-        let devpack_path = out_dir.join(inner_name);
-        let _ = std::fs::remove_dir_all(&devpack_path);
-        zip_content
-            .extract(&out_dir)
-            .context("Failed to extract devpack to directory")?;
-        Ok(devpack_path)
-    }
-
-    download(&zip_name, false)
-        .or_else(|_| download(&zip_name, true))
-        .map(DevelPack)
-}
-
-fn get_defines(is_debug: bool) -> Vec<(&'static str, &'static str)> {
-    vec![
-        ("ZEND_WIN32", "1"),
-        ("PHP_WIN32", "1"),
-        ("WINDOWS", "1"),
-        ("WIN32", "1"),
-        ("ZEND_DEBUG", if is_debug { "1" } else { "0" }),
-    ]
-}
-
 fn build_wrapper(defines: &[(&str, &str)], includes: &[PathBuf]) -> Result<()> {
     let mut build = cc::Build::new();
     for (var, val) in defines {
@@ -242,13 +348,7 @@ fn build_wrapper(defines: &[(&str, &str)], includes: &[PathBuf]) -> Result<()> {
     Ok(())
 }
 
-fn generate_bindings(
-    defines: &[(&str, &str)],
-    includes: &[PathBuf],
-    php_lib_name: &str,
-) -> Result<()> {
-    let out_dir = env::var_os("OUT_DIR").context("Failed to get OUT_DIR")?;
-    let out_path = PathBuf::from(out_dir).join("bindings.rs");
+fn generate_bindings(defines: &[(&str, &str)], includes: &[PathBuf]) -> Result<String> {
     let mut bindgen = bindgen::Builder::default()
         .header("src/wrapper.h")
         .clang_args(
@@ -283,27 +383,34 @@ fn generate_bindings(
         .map_err(|_| anyhow!("Unable to generate bindings for PHP"))?
         .to_string();
 
-    // For some reason some symbols don't link without a `#[link(name = "php8")]`
-    // attribute on each extern block. Bindgen doesn't give us the option to add
-    // this so we need to add it manually.
+    Ok(bindings)
+}
+
+fn check_php_version(info: &PHPInfo) -> Result<()> {
+    let version = info.zend_version()?;
+
+    if !(MIN_PHP_API_VER..=MAX_PHP_API_VER).contains(&version) {
+        bail!("The current version of PHP is not supported. Current PHP API version: {}, requires a version between {} and {}", version, MIN_PHP_API_VER, MAX_PHP_API_VER);
+    }
+
+    // Infra cfg flags - use these for things that change in the Zend API that don't
+    // rely on a feature and the crate user won't care about (e.g. struct field
+    // changes). Use a feature flag for an actual feature (e.g. enums being
+    // introduced in PHP 8.1).
     //
-    // Also, some functions need to be vectorcall. Primarily array functions.
-    let out_file = File::create(&out_path).context("Failed to open output bindings file")?;
-    let mut writer = BufWriter::new(out_file);
-    for line in bindings.lines() {
-        match &*line {
-            "extern \"C\" {" | "extern \"fastcall\" {" => {
-                writeln!(&mut writer, "#[link(name = \"{}\")]", php_lib_name)?;
-            }
-            _ => {}
-        }
-        writeln!(&mut writer, "{}", line)?;
+    // PHP 8.0 is the baseline - no feature flags will be introduced here.
+    //
+    // The PHP version cfg flags should also stack - if you compile on PHP 8.2 you
+    // should get both the `php81` and `php82` flags.
+    const PHP_81_API_VER: u32 = 20210902;
+
+    if version >= PHP_81_API_VER {
+        println!("cargo:rustc-cfg=php81");
     }
 
     Ok(())
 }
 
-#[cfg(windows)]
 fn main() -> Result<()> {
     let manifest: PathBuf = std::env::var("CARGO_MANIFEST_DIR").unwrap().into();
     for path in [
@@ -316,37 +423,74 @@ fn main() -> Result<()> {
 
     let php = find_php()?;
     let info = PHPInfo::get(&php)?;
-    let arch = info.architecture()?;
-    let is_zts = info.thread_safety()?;
-    let version = info.version()?;
-    let debug = info.debug()?;
-    let devkit = download_devel_pack(version, is_zts, arch)?;
-    let includes = devkit.include_paths();
-    let defines = get_defines(debug);
-    let php_lib = devkit.php_lib();
-    let php_lib_name = php_lib
-        .file_stem()
-        .context("Failed to get PHP library name")?
-        .to_string_lossy();
 
+    #[cfg(windows)]
+    let provider = Windows::new(&info)?;
+    #[cfg(not(windows))]
+    let provider = Unix::new(&info)?;
+
+    let includes = provider.get_includes()?;
+    let defines = provider.get_defines()?;
+
+    check_php_version(&info)?;
     build_wrapper(&defines, &includes)?;
-    generate_bindings(&defines, &includes, &*php_lib_name)?;
+    let bindings = generate_bindings(&defines, &includes)?;
 
-    println!("cargo:rustc-cfg=php81");
-    if debug {
+    let out_dir = env::var_os("OUT_DIR").context("Failed to get OUT_DIR")?;
+    let out_path = PathBuf::from(out_dir).join("bindings.rs");
+    let out_file =
+        File::create(&out_path).context("Failed to open output bindings file for writing")?;
+    let mut out_writer = BufWriter::new(out_file);
+    provider.write_bindings(bindings, &mut out_writer)?;
+
+    if info.debug()? {
         println!("cargo:rustc-cfg=php_debug");
     }
-    println!("cargo:rustc-link-lib=dylib={}", php_lib_name);
-    println!(
-        "cargo:rustc-link-search={}",
-        php_lib
-            .parent()
-            .context("Failed to get PHP library parent directory")?
-            .to_string_lossy()
-    );
+    if info.thread_safety()? {
+        println!("cargo:rustc-cfg=php_zts");
+    }
+    provider.print_extra_link_args()?;
 
     Ok(())
 }
+
+// #[cfg(windows)]
+// fn main() -> Result<()> {
+//     return Ok(());
+
+//     let php = find_php()?;
+//     let info = PHPInfo::get(&php)?;
+//     let arch = info.architecture()?;
+//     let is_zts = info.thread_safety()?;
+//     let version = info.version()?;
+//     let debug = info.debug()?;
+//     let devkit = download_devel_pack(version, is_zts, arch)?;
+//     let includes = devkit.include_paths();
+//     let defines = get_defines(debug);
+//     let php_lib = devkit.php_lib();
+//     let php_lib_name = php_lib
+//         .file_stem()
+//         .context("Failed to get PHP library name")?
+//         .to_string_lossy();
+
+//     build_wrapper(&defines, &includes)?;
+//     generate_bindings(&defines, &includes, &*php_lib_name)?;
+
+//     println!("cargo:rustc-cfg=php81");
+//     if debug {
+//         println!("cargo:rustc-cfg=php_debug");
+//     }
+//     println!("cargo:rustc-link-lib=dylib={}", php_lib_name);
+//     println!(
+//         "cargo:rustc-link-search={}",
+//         php_lib
+//             .parent()
+//             .context("Failed to get PHP library parent directory")?
+//             .to_string_lossy()
+//     );
+
+//     Ok(())
+// }
 
 #[cfg(not(windows))]
 fn main() {
@@ -459,7 +603,7 @@ fn main() {
         .write_to_file(out_path)
         .expect("Unable to write bindings file.");
 
-    let configure = Configure::get();
+    let configure = PHPConfig::get();
 
     if configure.has_zts() {
         println!("cargo:rustc-cfg=php_zts");
@@ -485,6 +629,7 @@ impl PHPInfo {
         Ok(Self(stdout.to_string()))
     }
 
+    #[cfg(windows)]
     pub fn architecture(&self) -> Result<&str> {
         self.get_key("Architecture")
             .context("Could not find architecture of PHP")
@@ -509,6 +654,12 @@ impl PHPInfo {
             .context("Failed to get PHP version")
     }
 
+    pub fn zend_version(&self) -> Result<u32> {
+        self.get_key("PHP API")
+            .context("Failed to get Zend version")
+            .and_then(|s| u32::from_str(s).context("Failed to convert Zend version to integer"))
+    }
+
     fn get_key(&self, key: &str) -> Option<&str> {
         let split = format!("{} => ", key);
         for line in self.0.lines() {
@@ -518,36 +669,6 @@ impl PHPInfo {
             }
         }
         None
-    }
-}
-
-struct Configure(String);
-
-impl Configure {
-    pub fn get() -> Self {
-        let cmd = Command::new("php-config")
-        .arg("--configure-options")
-        .output()
-        .expect("Unable to run `php-config --configure-options`. Please ensure it is visible in your PATH.");
-
-        if !cmd.status.success() {
-            let stderr = String::from_utf8(cmd.stderr)
-                .unwrap_or_else(|_| String::from("Unable to read stderr"));
-            panic!("Error running `php -i`: {}", stderr);
-        }
-
-        // check for the ZTS feature flag in configure
-        let stdout =
-            String::from_utf8(cmd.stdout).expect("Unable to read stdout from `php-config`.");
-        Self(stdout)
-    }
-
-    pub fn has_zts(&self) -> bool {
-        self.0.contains("--enable-zts")
-    }
-
-    pub fn debug(&self) -> bool {
-        self.0.contains("--enable-debug")
     }
 }
 
