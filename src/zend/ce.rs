e7da56656dbf37a633418e97a2d65bbde339592a
Niklas Mollenhauer <nikeee@outlook.com>
2022-10-15 15:27:09 +0200

    Add some standard zend interfaces
    The zend API also has some standard interfaces it exposes:
    https://github.com/php/php-src/blob/c8c09b4aaee7ac447828564c6267e62eb304135b/Zend/zend_interfaces.h#L27-L33
    ```
    extern ZEND_API zend_class_entry *zend_ce_traversable;
    extern ZEND_API zend_class_entry *zend_ce_aggregate;
    extern ZEND_API zend_class_entry *zend_ce_iterator;
    extern ZEND_API zend_class_entry *zend_ce_arrayaccess;
    extern ZEND_API zend_class_entry *zend_ce_serializable;
    extern ZEND_API zend_class_entry *zend_ce_countable;
    extern ZEND_API zend_class_entry *zend_ce_stringable;
    ```
    
    This surfaced in #163 and should make it possible to implement these interfaces.

--- src/zend/ce.rs
+++ src/zend/ce.rs
@@ -3,10 +3,12 @@
 #![allow(clippy::unwrap_used)]
 
 use crate::ffi::{
-    zend_ce_argument_count_error, zend_ce_arithmetic_error, zend_ce_compile_error,
-    zend_ce_division_by_zero_error, zend_ce_error_exception, zend_ce_exception,
-    zend_ce_parse_error, zend_ce_throwable, zend_ce_type_error, zend_ce_unhandled_match_error,
-    zend_ce_value_error, zend_standard_class_def,
+    zend_ce_aggregate, zend_ce_argument_count_error, zend_ce_arithmetic_error, zend_ce_arrayaccess,
+    zend_ce_compile_error, zend_ce_countable, zend_ce_division_by_zero_error,
+    zend_ce_error_exception, zend_ce_exception, zend_ce_iterator, zend_ce_parse_error,
+    zend_ce_serializable, zend_ce_stringable, zend_ce_throwable, zend_ce_traversable,
+    zend_ce_type_error, zend_ce_unhandled_match_error, zend_ce_value_error,
+    zend_standard_class_def,
 };
 
 use super::ClassEntry;
@@ -69,4 +71,39 @@ pub fn division_by_zero_error() -> &'static ClassEntry {
 /// Returns the base `UnhandledMatchError` class.
 pub fn unhandled_match_error() -> &'static ClassEntry {
     unsafe { zend_ce_unhandled_match_error.as_ref() }.unwrap()
+}
+
+/// Returns the [`Traversable`](https://www.php.net/manual/en/class.traversable.php) interface.
+pub fn traversable() -> &'static ClassEntry {
+    unsafe { zend_ce_traversable.as_ref() }.unwrap()
+}
+
+/// Returns the [`IteratorAggregate`](https://www.php.net/manual/en/class.iteratoraggregate.php) interface.
+pub fn aggregate() -> &'static ClassEntry {
+    unsafe { zend_ce_aggregate.as_ref() }.unwrap()
+}
+
+/// Returns the [`Iterator`](https://www.php.net/manual/en/class.iterator.php) interface.
+pub fn iterator() -> &'static ClassEntry {
+    unsafe { zend_ce_iterator.as_ref() }.unwrap()
+}
+
+/// Returns the [`ArrayAccess`](https://www.php.net/manual/en/class.arrayaccess.php) interface.
+pub fn arrayaccess() -> &'static ClassEntry {
+    unsafe { zend_ce_arrayaccess.as_ref() }.unwrap()
+}
+
+/// Returns the [`Serializable`](https://www.php.net/manual/en/class.serializable.php) interface.
+pub fn serializable() -> &'static ClassEntry {
+    unsafe { zend_ce_serializable.as_ref() }.unwrap()
+}
+
+/// Returns the [`Countable`](https://www.php.net/manual/en/class.countable.php) interface.
+pub fn countable() -> &'static ClassEntry {
+    unsafe { zend_ce_countable.as_ref() }.unwrap()
+}
+
+/// Returns the [`Stringable`](https://www.php.net/manual/en/class.stringable.php) interface.
+pub fn stringable() -> &'static ClassEntry {
+    unsafe { zend_ce_stringable.as_ref() }.unwrap()
 }
