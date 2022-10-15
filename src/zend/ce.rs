ae04fd05f6ae560bed330d5d293e9603301ba35c
Niklas Mollenhauer <nikeee@outlook.com>
2022-10-15 15:31:41 +0200

    Add some links to the php documentation

--- src/zend/ce.rs
+++ src/zend/ce.rs
@@ -13,62 +13,62 @@ use crate::ffi::{
 
 use super::ClassEntry;
 
-/// Returns the base `stdClass` class.
+/// Returns the base [`stdClass`](https://www.php.net/manual/en/class.stdclass.php) class.
 pub fn stdclass() -> &'static ClassEntry {
     unsafe { zend_standard_class_def.as_ref() }.unwrap()
 }
 
-/// Returns the base `Throwable` class.
+/// Returns the base [`Throwable`](https://www.php.net/manual/en/class.throwable.php) class.
 pub fn throwable() -> &'static ClassEntry {
     unsafe { zend_ce_throwable.as_ref() }.unwrap()
 }
 
-/// Returns the base `Exception` class.
+/// Returns the base [`Exception`](https://www.php.net/manual/en/class.exception.php) class.
 pub fn exception() -> &'static ClassEntry {
     unsafe { zend_ce_exception.as_ref() }.unwrap()
 }
 
-/// Returns the base `ErrorException` class.
+/// Returns the base [`ErrorException`](https://www.php.net/manual/en/class.errorexception.php) class.
 pub fn error_exception() -> &'static ClassEntry {
     unsafe { zend_ce_error_exception.as_ref() }.unwrap()
 }
 
-/// Returns the base `CompileError` class.
+/// Returns the base [`CompileError`](https://www.php.net/manual/en/class.compileerror.php) class.
 pub fn compile_error() -> &'static ClassEntry {
     unsafe { zend_ce_compile_error.as_ref() }.unwrap()
 }
 
-/// Returns the base `ParseError` class.
+/// Returns the base [`ParseError`](https://www.php.net/manual/en/class.parseerror.php) class.
 pub fn parse_error() -> &'static ClassEntry {
     unsafe { zend_ce_parse_error.as_ref() }.unwrap()
 }
 
-/// Returns the base `TypeError` class.
+/// Returns the base [`TypeError`](https://www.php.net/manual/en/class.typeerror.php) class.
 pub fn type_error() -> &'static ClassEntry {
     unsafe { zend_ce_type_error.as_ref() }.unwrap()
 }
 
-/// Returns the base `ArgumentCountError` class.
+/// Returns the base [`ArgumentCountError`](https://www.php.net/manual/en/class.argumentcounterror.php) class.
 pub fn argument_count_error() -> &'static ClassEntry {
     unsafe { zend_ce_argument_count_error.as_ref() }.unwrap()
 }
 
-/// Returns the base `ValueError` class.
+/// Returns the base [`ValueError`](https://www.php.net/manual/en/class.valueerror.php) class.
 pub fn value_error() -> &'static ClassEntry {
     unsafe { zend_ce_value_error.as_ref() }.unwrap()
 }
 
-/// Returns the base `ArithmeticError` class.
+/// Returns the base [`ArithmeticError`](https://www.php.net/manual/en/class.arithmeticerror.php) class.
 pub fn arithmetic_error() -> &'static ClassEntry {
     unsafe { zend_ce_arithmetic_error.as_ref() }.unwrap()
 }
 
-/// Returns the base `DivisionByZeroError` class.
+/// Returns the base [`DivisionByZeroError`](https://www.php.net/manual/en/class.divisionbyzeroerror.php) class.
 pub fn division_by_zero_error() -> &'static ClassEntry {
     unsafe { zend_ce_division_by_zero_error.as_ref() }.unwrap()
 }
 
-/// Returns the base `UnhandledMatchError` class.
+/// Returns the base [`UnhandledMatchError`](https://www.php.net/manual/en/class.unhandledmatcherror.php) class.
 pub fn unhandled_match_error() -> &'static ClassEntry {
     unsafe { zend_ce_unhandled_match_error.as_ref() }.unwrap()
 }
