c3900aae34979cbc43c2dfe845acbdd7845c7776
Daniil Gentili <daniil@daniil.it>
2023-11-24 23:01:19 +0100

    Revert "Revert "Add integration tests""

---
+++ tests/src/integration/array.php
@@ -0,0 +1,26 @@
+<?php
+
+require('_utils.php');
+
+// Tests sequential arrays
+$array = test_array(['a', 'b', 'c']);
+
+assert(is_array($array));
+assert(count($array) === 3);
+assert(in_array('a', $array));
+assert(in_array('b', $array));
+assert(in_array('c', $array));
+
+// Tests associative arrays
+$assoc = test_array_assoc([
+    'a' => '1',
+    'b' => '2',
+    'c' => '3'
+]);
+
+assert(array_key_exists('a', $assoc));
+assert(array_key_exists('b', $assoc));
+assert(array_key_exists('c', $assoc));
+assert(in_array('1', $assoc));
+assert(in_array('2', $assoc));
+assert(in_array('3', $assoc));
