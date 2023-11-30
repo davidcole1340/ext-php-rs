e2dc601ff299865ea2582685f0323f358a222e3d
Daniil Gentili <daniil@daniil.it>
2023-11-30 22:42:16 +0100

    Test type errors

--- tests/src/integration/closure.php
+++ tests/src/integration/closure.php
@@ -12,3 +12,11 @@ $closure = test_closure_once('test');
 
 assert(call_user_func($closure) === 'test');
 assert_exception_thrown($closure);
+
+function take(\stdClass $rs): void { }
+
+try {
+    take($closure);
+} catch (\TypeError $e) {
+    assert(str_starts_with($e->getMessage(), 'take(): Argument #1 ($rs) must be of type stdClass, RustClosure given, called in '));
+}
