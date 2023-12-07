cb2afe9e26715cef33c2aa4f48ff43574dcb8aff
Daniil Gentili <daniil@daniil.it>
2023-12-07 19:02:54 +0100

    Add tests

---
+++ tests/src/integration/types.php
@@ -0,0 +1,40 @@
+<?php
+
+require('_utils.php');
+
+const TYPES = [
+    'test_str' => [['string'], 'string'],
+    'test_string' => [['string'], 'string'],
+    'test_bool' => [['bool'], 'bool'],
+    'test_number_signed' => [['int'], 'int'],
+    'test_number_unsigned' => [['int'], 'int'],
+    'test_number_float' => [['float'], 'float'],
+    'test_array' => [['array'], 'array'],
+    'test_array' => [['array'], 'array'],
+    'test_array_assoc' => [['array'], 'array'],
+    'test_binary' => [['string'], 'string'],
+    'test_nullable' => [['?string'], '?string'],
+    'test_object' => [['object'], 'object'],
+    'test_closure' => [[], 'RustClosure'],
+    'test_closure_once' => [['string'], 'RustClosure'],
+    'test_callable' => [['callable', 'string'], 'mixed']
+];
+
+function toStr(ReflectionNamedType|ReflectionUnionType|ReflectionIntersectionType|null $v): string {
+    if ($v === null) {
+        return '<null>';
+    }
+    return match (true) {
+        $v instanceof ReflectionNamedType => $v->allowsNull() ? '?'.$v->getName() : $v->getName(),
+        $v instanceof ReflectionUnionType => $v->getName(),
+        $v instanceof ReflectionIntersectionType => $v->getName(),
+    };
+}
+
+foreach (TYPES as $func => [$args, $return]) {
+    $f = new ReflectionFunction($func);
+    assert(toStr($f->getReturnType()) === $return, "Wrong return type of $func, expected $return, got ".((string)$f->getReturnType()));
+    foreach ($f->getParameters() as $idx => $param) {
+        assert(toStr($param->getType()) === $args[$idx], "Wrong arg type $idx of $func, expected {$args[$idx]}, got ".((string)$param->getType()));
+    }
+}
---
+++ tests/src/integration/types.rs
@@ -0,0 +1,4 @@
+#[test]
+fn types_work() {
+    assert!(crate::integration::run_php("types.php"));
+}
