a62fbba0b29b8b7fca11af529c7d7a9d7402b976
Xenira <1288524+Xenira@users.noreply.github.com>
2025-01-09 22:31:01 +0100

    fix(array): fix double ended iterator implementation
    Refs: #316

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,5 +1,9 @@
 #![cfg_attr(windows, feature(abi_vectorcall))]
-use ext_php_rs::{binary::Binary, prelude::*, types::ZendObject, types::Zval};
+use ext_php_rs::{
+    binary::Binary,
+    prelude::*,
+    types::{ArrayKey, ZendHashTable, ZendObject, Zval},
+};
 use std::collections::HashMap;
 
 #[php_function]
@@ -70,6 +74,59 @@ pub fn test_closure_once(a: String) -> Closure {
 #[php_function]
 pub fn test_callable(call: ZendCallable, a: String) -> Zval {
     call.try_call(vec![&a]).expect("Failed to call function")
+}
+
+#[php_function]
+pub fn iter_next(ht: &ZendHashTable) -> Vec<Zval> {
+    ht.iter()
+        .flat_map(|(k, v)| [key_to_zval(k), v.shallow_clone()])
+        .collect()
+}
+
+#[php_function]
+pub fn iter_back(ht: &ZendHashTable) -> Vec<Zval> {
+    ht.iter()
+        .rev()
+        .flat_map(|(k, v)| [key_to_zval(k), v.shallow_clone()])
+        .collect()
+}
+
+#[php_function]
+pub fn iter_next_back(ht: &ZendHashTable, modulus: usize) -> Vec<Option<Zval>> {
+    let mut result = Vec::with_capacity(ht.len());
+    let mut iter = ht.iter();
+
+    for i in 0..ht.len() + modulus {
+        let entry = if i % modulus == 0 {
+            iter.next_back()
+        } else {
+            iter.next()
+        };
+
+        if let Some((k, v)) = entry {
+            result.push(Some(key_to_zval(k)));
+            result.push(Some(v.shallow_clone()));
+        } else {
+            result.push(None);
+        }
+    }
+
+    result
+}
+
+fn key_to_zval(key: ArrayKey) -> Zval {
+    match key {
+        ArrayKey::String(s) => {
+            let mut zval = Zval::new();
+            let _ = zval.set_string(s.as_str(), false);
+            zval
+        }
+        ArrayKey::Long(l) => {
+            let mut zval = Zval::new();
+            zval.set_long(l);
+            zval
+        }
+    }
 }
 
 #[php_class]
@@ -179,6 +236,7 @@ mod integration {
     mod callable;
     mod class;
     mod closure;
+    mod iterator;
     mod nullable;
     mod number;
     mod object;
