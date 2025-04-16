201990efd6d0d3db2e9e305f8b0b611bf31c3fed
Alexsander <13504897+Norbytus@users.noreply.github.com>
2025-04-16 23:15:13 +0300

    refactor(macro): trait rename for general and method names
    * refactor(macro): Make rename as Trait && separate to common rename, method_rename
    
    * test(rename): add explicit tests for rename
    
    Refs: #420
    
    * fix(macro): Fix rename some magic methods
    
    * test(macro): Add test for magic method call
    
    * refactor(macro): Change if condition
    
    * test(macro): Fix tests
    
    ---------
    
    Co-authored-by: Xenira <1288524+Xenira@users.noreply.github.com>

---
+++ tests/src/integration/magic_method.rs
@@ -0,0 +1,4 @@
+#[test]
+fn magic_method() {
+    assert!(crate::integration::run_php("magic_method.php"));
+}
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -225,10 +225,101 @@ pub fn test_class(string: String, number: i32) -> TestClass {
     }
 }
 
+#[php_class]
+pub struct MagicMethod(i64);
+
+#[php_impl]
+impl MagicMethod {
+    pub fn __construct() -> Self {
+        Self(0)
+    }
+
+    pub fn __destruct(&self) {}
+
+    pub fn __call(&self, name: String, _arguments: HashMap<String, &Zval>) -> Zval {
+        let mut z = Zval::new();
+        if name == "callMagicMethod" {
+            let s = "Hello".to_string();
+
+            let _ = z.set_string(s.as_str(), false);
+            z
+        } else {
+            z.set_null();
+            z
+        }
+    }
+
+    pub fn __call_static(name: String, arguments: HashMap<String, &Zval>) -> Zval {
+        let mut zval = Zval::new();
+        if name == "callStaticSomeMagic" {
+            let concat_args = format!(
+                "Hello from static call {}",
+                arguments
+                    .iter()
+                    .filter(|(_, v)| v.is_long())
+                    .map(|(_, s)| s.long().unwrap())
+                    .collect::<Vec<_>>()
+                    .iter()
+                    .sum::<i64>()
+            );
+
+            let _ = zval.set_string(&concat_args, false);
+            zval
+        } else {
+            zval.set_null();
+            zval
+        }
+    }
+
+    pub fn __get(&self, name: String) -> Zval {
+        let mut v = Zval::new();
+        v.set_null();
+        if name == "count" {
+            v.set_long(self.0);
+        }
+
+        v
+    }
+
+    pub fn __set(&mut self, prop_name: String, val: &Zval) {
+        if val.is_long() && prop_name == "count" {
+            self.0 = val.long().unwrap()
+        }
+    }
+
+    pub fn __isset(&self, prop_name: String) -> bool {
+        "count" == prop_name
+    }
+
+    pub fn __unset(&mut self, prop_name: String) {
+        if prop_name == "count" {
+            self.0 = 0;
+        }
+    }
+
+    pub fn __to_string(&self) -> String {
+        self.0.to_string()
+    }
+
+    pub fn __invoke(&self, n: i64) -> i64 {
+        self.0 + n
+    }
+
+    pub fn __debug_info(&self) -> HashMap<String, Zval> {
+        let mut h: HashMap<String, Zval> = HashMap::new();
+        let mut z = Zval::new();
+        z.set_long(self.0);
+        h.insert("count".to_string(), z);
+
+        h
+    }
+}
+
 #[php_module]
 pub fn build_module(module: ModuleBuilder) -> ModuleBuilder {
     module
         .class::<TestClass>()
+        .class::<MagicMethod>()
         .function(wrap_function!(test_str))
         .function(wrap_function!(test_string))
         .function(wrap_function!(test_bool))
@@ -321,6 +412,7 @@ mod integration {
     mod closure;
     mod globals;
     mod iterator;
+    mod magic_method;
     mod nullable;
     mod number;
     mod object;
