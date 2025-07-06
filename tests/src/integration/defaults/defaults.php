501f7149bdf32e9a8ad4f951e752aad4ad0692fe
Dmitry Dygalo <dmitry@dygalo.dev>
2025-07-06 05:08:07 -0700

    fix: support defaults for calls with named arguments
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>
    Fixes: #490

--- tests/src/integration/defaults/defaults.php
+++ tests/src/integration/defaults/defaults.php
@@ -4,3 +4,6 @@ assert(test_defaults_integer() === 42);
 assert(test_defaults_integer(12) === 12);
 assert(test_defaults_nullable_string() === null);
 assert(test_defaults_nullable_string('test') === 'test');
+assert(test_defaults_multiple_option_arguments() === 'Default');
+assert(test_defaults_multiple_option_arguments(a: 'a') === 'a');
+assert(test_defaults_multiple_option_arguments(b: 'b') === 'b');
