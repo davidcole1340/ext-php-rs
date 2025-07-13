bcc175d37f4b7473ea6fb5f548dcc217e6a3e53e
norbytus <norbyt93@gmail.com>
2025-07-13 22:10:45 +0300

    feat: Add tests

---
+++ tests/src/integration/interface/interface.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/* assert(interface_exists('ExtPhpRs\Interface\EmptyObjectInterface'), 'Interface not exist'); */
+
+final readonly class InterfaceExampleUsage implements ExtPhpRs\Interface\EmptyObjectInterface
+{
+
+}
+
+/* $example = new InterfaceExampleUsage; */
+/* assert( */
+/* 	is_a($example, ExtPhpRs\Interface\EmptyObjectInterface::class), */
+/* 	\sprintf( */
+/* 		'Class should be implements of interface: %s', */
+/* 		Test\TestInterface::class */
+/* 	) */
+/* ); */
