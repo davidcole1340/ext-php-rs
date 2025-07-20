0011922221e242a09b121b8f3fa19b665a0dcb8a
norbytus <norbyt93@gmail.com>
2025-07-20 17:15:42 +0300

    feat: Add methods, const registration for interface

--- tests/src/integration/interface/mod.rs
+++ tests/src/integration/interface/mod.rs
@@ -1,18 +1,31 @@
-use ext_php_rs::prelude::*;
 use std::collections::HashMap;
 
+use ext_php_rs::types::ZendClassObject;
+use ext_php_rs::php_interface;
+use ext_php_rs::{php_module, prelude::ModuleBuilder};
+use ext_php_rs::zend::ce;
+
 #[php_interface]
+#[php(extends(ce = ce::throwable, stub = "\\Throwable"))]
 #[php(name = "ExtPhpRs\\Interface\\EmptyObjectInterface")]
-pub trait EmptyObjectInterface { }
+pub trait EmptyObjectTrait
+{
+    const HELLO: &'static str = "HELLO";
 
-pub fn build_module(builder: ModuleBuilder) -> ModuleBuilder {
-    builder.interface::<PhpInterfaceEmptyObjectInterface>()
+    fn void();
+
+    fn non_static(&self, data: String) -> String;
+
+    fn ref_to_like_this_class(
+        &self,
+        data: String,
+        other: &ZendClassObject<PhpInterfaceEmptyObjectTrait>
+    ) -> String;
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn interface_work() {
-        assert!(crate::integration::test::run_php("interface/interface.php"));
-    }
+#[php_module]
+pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
+    module
+        .interface::<PhpInterfaceEmptyObjectTrait>()
 }
+
