c2ed4b135fa843a7959f6a2c842c71e9449ace76
norbytus <norbyt93@gmail.com>
2025-08-04 17:39:55 +0300

    fix: Fix constant registration for interface

--- tests/src/integration/interface/mod.rs
+++ tests/src/integration/interface/mod.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use ext_php_rs::types::ZendClassObject;
 use ext_php_rs::zend::ce;
 use ext_php_rs::{php_class, php_impl, php_interface};
@@ -9,6 +7,10 @@ use ext_php_rs::{php_module, prelude::ModuleBuilder};
 #[php(extends(ce = ce::throwable, stub = "\\Throwable"))]
 #[php(name = "ExtPhpRs\\Interface\\EmptyObjectInterface")]
 pub trait EmptyObjectTrait {
+    const STRING_CONST: &'static str = "STRING_CONST";
+
+    const USIZE_CONST: u64 = 200;
+
     fn void();
 
     fn non_static(&self, data: String) -> String;
