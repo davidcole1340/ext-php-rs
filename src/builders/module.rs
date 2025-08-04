fc00fb5a178e5eb05ff596ad534b03595cca6a62
norbytus <norbyt93@gmail.com>
2025-08-04 17:40:09 +0300

    feat: Separate interfaces from classes in module

--- src/builders/module.rs
+++ src/builders/module.rs
@@ -48,6 +48,7 @@ pub struct ModuleBuilder<'a> {
     pub(crate) functions: Vec<FunctionBuilder<'a>>,
     pub(crate) constants: Vec<(String, Box<dyn IntoConst + Send>, DocComments)>,
     pub(crate) classes: Vec<fn() -> ClassBuilder>,
+    pub(crate) interfaces: Vec<fn() -> ClassBuilder>,
     #[cfg(feature = "enum")]
     pub(crate) enums: Vec<fn() -> EnumBuilder>,
     startup_func: Option<StartupShutdownFunc>,
@@ -198,7 +199,7 @@ impl ModuleBuilder<'_> {
     ///
     /// * Panics if a constant could not be registered.
     pub fn interface<T: RegisteredClass>(mut self) -> Self {
-        self.classes.push(|| {
+        self.interfaces.push(|| {
             let mut builder = InterfaceBuilder::new(T::CLASS_NAME);
             for (method, flags) in T::method_builders() {
                 builder = builder.method(method, flags);
@@ -298,6 +299,7 @@ impl ModuleBuilder<'_> {
 pub struct ModuleStartup {
     constants: Vec<(String, Box<dyn IntoConst + Send>)>,
     classes: Vec<fn() -> ClassBuilder>,
+    interfaces: Vec<fn() -> ClassBuilder>,
     #[cfg(feature = "enum")]
     enums: Vec<fn() -> EnumBuilder>,
 }
@@ -320,6 +322,10 @@ impl ModuleStartup {
 
         self.classes.into_iter().map(|c| c()).for_each(|c| {
             c.register().expect("Failed to build class");
+        });
+
+        self.interfaces.into_iter().map(|c| c()).for_each(|c| {
+            c.register().expect("Failed to build interface");
         });
 
         #[cfg(feature = "enum")]
@@ -364,6 +370,7 @@ impl TryFrom<ModuleBuilder<'_>> for (ModuleEntry, ModuleStartup) {
                 .map(|(n, v, _)| (n, v))
                 .collect(),
             classes: builder.classes,
+            interfaces: builder.interfaces,
             #[cfg(feature = "enum")]
             enums: builder.enums,
         };
@@ -419,6 +426,7 @@ mod tests {
         assert!(builder.functions.is_empty());
         assert!(builder.constants.is_empty());
         assert!(builder.classes.is_empty());
+        assert!(builder.interfaces.is_empty());
         assert!(builder.startup_func.is_none());
         assert!(builder.shutdown_func.is_none());
         assert!(builder.request_startup_func.is_none());
